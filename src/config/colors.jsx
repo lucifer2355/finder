a306b153f5f2ff518b846e175c5ec0a5482c693a
lucifer2355 <dgajjar999@gmail.com>
2021-08-07 15:25:06 +0530

    add colors

---
+++ src/config/colors.jsx
@@ -0,0 +1,8 @@
+export const colors = Object.freeze({
+    primary: "#FF6B3D",
+    secondary: "#42BD41",
+    gray: '#505050',
+    white: "#FFFFFF",
+    blue: '#6989FF',
+    lightBlue: '#1CB1E9' 
+})
