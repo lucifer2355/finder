70deb2a63a9679f006d5dc98bf7c735233d0e704
lucifer2355 <dgajjar999@gmail.com>
2021-08-08 12:50:29 +0530

    add bottom navigation tab icons

--- src/navigations/AppNavigator.jsx
+++ src/navigations/AppNavigator.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 import { colors } from "../config/colors";
 import HomeNavigator from "./HomeNavigator";
+import SearchNavigator from "./SearchNavigator";
+import NotificationNavigator from "./NotificationNavigator";
+import ProfileNavigator from "./ProfileNavigator";
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -12,22 +16,42 @@ const AppNavigator = () => (
   <NavigationContainer>
     <Tab.Navigator
       initialRouteName='Home'
-      tabBarOptions={{
-        activeTintColor: colors.blue,
-        inactiveTintColor: colors.white,
-        showLabel: false,
-        keyboardHidesTabBar: true,
-        style: {
-          backgroundColor: colors.primary,
-        },
-      }}
+      activeColor={colors.white}
+      barStyle={{ backgroundColor: colors.primary }}
     >
       <Tab.Screen
         name='Home'
         component={HomeNavigator}
         options={{
-          tabBarIcon: ({ color, size }) => (
-            <Icon name='home' color={color} size={size} />
+          tabBarIcon: ({ color }) => (
+            <Icon name='home' color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Search'
+        component={SearchNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name='search' color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Notification'
+        component={NotificationNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name='notifications' color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Profile'
+        component={ProfileNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Icon name='account' color={color} size={26} />
           ),
         }}
       />
