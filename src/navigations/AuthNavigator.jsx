0918d39d7b7f547e37a7b6c688bbc11288a1d6cf
lucifer2355 <dgajjar999@gmail.com>
2021-08-08 23:54:16 +0530

    add header style in auth navigator

--- src/navigations/AuthNavigator.jsx
+++ src/navigations/AuthNavigator.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import { colors } from "../config/colors";
 import WelcomeScreen from "../screens/WelcomeScreen";
 import LoginScreen from "../screens/LoginScreen";
 import RegistrationScreen from "../screens/RegistrationScreen";
@@ -10,10 +11,29 @@ const Stack = createNativeStackNavigator();
 
 const AuthNavigator = () => (
   <NavigationContainer>
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name='Welcome' component={WelcomeScreen} />
-      <Stack.Screen name='Login' component={LoginScreen} />
-      <Stack.Screen name='Register' component={RegistrationScreen} />
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.primary,
+        },
+        headerTintColor: colors.white,
+      }}
+    >
+      <Stack.Screen
+        name='Welcome'
+        component={WelcomeScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name='Login'
+        component={LoginScreen}
+        options={{ headerBackTitleVisible: false }}
+      />
+      <Stack.Screen
+        name='Register'
+        component={RegistrationScreen}
+        options={{ headerBackTitleVisible: false }}
+      />
     </Stack.Navigator>
   </NavigationContainer>
 );
--- src/screens/RegistrationScreen.jsx
+++ src/screens/RegistrationScreen.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useLayoutEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-const RegistrationScreen = () => {
-    return (
-        <View style={styles.screen}>
-            <Text>Registration Screen</Text>
-        </View>
-    )
-}
+const RegistrationScreen = ({ navigation }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Create Account",
+    });
+  }, []);
+
+  return (
+    <View style={styles.screen}>
+      <Text>Registration Screen</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
-export default RegistrationScreen
-
+export default RegistrationScreen;
