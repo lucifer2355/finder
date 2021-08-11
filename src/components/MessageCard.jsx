ef8dc596f99e16eca59c5ad8b5b2067e22ce6e2d
lucifer2355 <dgajjar999@gmail.com>
2021-08-11 17:35:27 +0530

    remove mapbox

--- src/components/MessageCard.jsx
+++ src/components/MessageCard.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
-import { hp, wp } from "../config/HeightWidth";
 import { colors } from "../config/colors";
 
 const MessageCard = () => {
