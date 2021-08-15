171611f673a76e9e4f8cd87b9fba1967ce3254f0
lucifer2355 <dgajjar999@gmail.com>
2021-08-15 22:17:12 +0530

    add search result listing style & sent request button

--- src/screens/SearchResultScreen.jsx
+++ src/screens/SearchResultScreen.jsx
@@ -1,7 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { useSelector } from "react-redux";
 import SvgUri from "react-native-svg-uri";
+import { ListItem, Avatar } from "react-native-elements";
+import AntDesign from "react-native-vector-icons/AntDesign";
+
+import { colors } from "../config/colors";
 
 const SearchResultScreen = () => {
   const { searchResult } = useSelector((state) => state.searchReducer);
@@ -16,13 +20,44 @@ const SearchResultScreen = () => {
     );
   }
 
+  const sentRequest = () => {
+    console.log("sent request");
+  };
+
   return (
     <View>
-      <Text>Search Result Screen</Text>
+      {searchResult.map((s) => (
+        <ListItem key={s.id} bottomDivider>
+          <Avatar
+            rounded
+            size={50}
+            icon={{ name: "user", color: "orange", type: "font-awesome" }}
+            source={{
+              uri: s?.profileImage,
+            }}
+            containerStyle={{ borderColor: colors.primary, borderWidth: 1 }}
+          />
+          <ListItem.Content>
+            <ListItem.Title style={styles.title}>{s.fullName}</ListItem.Title>
+            <ListItem.Subtitle>{`${s.age} Years, ${s.city}, ${s.state}, ${s.country}`}</ListItem.Subtitle>
+          </ListItem.Content>
+          <AntDesign
+            name='pluscircleo'
+            size={28}
+            color={colors.primary}
+            onPress={sentRequest}
+          />
+        </ListItem>
+      ))}
     </View>
   );
 };
 
-export default SearchResultScreen;
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
 
-const styles = StyleSheet.create({});
+export default SearchResultScreen;
