59f363a80ee0113328cbc62f13382e94b61c9025
lucifer2355 <dgajjar999@gmail.com>
2021-08-16 15:50:04 +0530

    add delete & accept request action

--- src/store/friendship/friendshipAction.jsx
+++ src/store/friendship/friendshipAction.jsx
@@ -24,3 +24,46 @@ export const sentRequest =
       console.log("Error in sent request", error);
     }
   };
+
+export const deleteRequest =
+  (loginUserId, deleteRequestId) => async (dispatch) => {
+    try {
+      await db
+        .collection("friendship")
+        .doc(loginUserId)
+        .collection("sentRequest")
+        .delete({
+          userId: deleteRequestId,
+        });
+      await db
+        .collection("friendship")
+        .doc(deleteRequestId)
+        .collection("receiveRequest")
+        .delete({
+          userId: loginUserId,
+        });
+    } catch (error) {
+      console.log("Error in delete request", error);
+    }
+  };
+
+export const acceptRequest =
+  (loginUserId, acceptRequestId) => async (dispatch) => {
+    try {
+      await db
+        .collection("friendship")
+        .doc(loginUserId)
+        .collection("friends")
+        .add({
+          userId: acceptRequest,
+        });
+      await db
+        .collection("friendship")
+        .doc(acceptRequestId)
+        .collection("friends")
+        .add({
+          userId: loginUserId,
+        });
+      dispatch(deleteRequest(loginUserId, acceptRequestId));
+    } catch (error) {}
+  };
