6857d76c36e45a545b2ea000cd4e5459c972489a
lucifer2355 <dgajjar999@gmail.com>
2021-08-21 19:31:46 +0530

    add tab bar style

--- src/navigations/NotificationNavigator.jsx
+++ src/navigations/NotificationNavigator.jsx
@@ -2,18 +2,33 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
-const Tab = createMaterialTopTabNavigator();
-
 import { colors } from "../config/colors";
 import NotificationScreen from "../screens/NotificationScreen";
 
 const Stack = createNativeStackNavigator();
+const Tab = createMaterialTopTabNavigator();
 
-const NotificationNavigator = () => (
-  <Tab.Navigator>
+const TabNavigator = () => (
+  <Tab.Navigator
+    screenOptions={{
+      tabBarLabelStyle: { fontSize: 14, fontWeight: "600" },
+      tabBarIndicatorStyle: { backgroundColor: colors.primary },
+    }}
+  >
     <Tab.Screen name='Home' component={NotificationScreen} />
     <Tab.Screen name='Settings' component={NotificationScreen} />
   </Tab.Navigator>
+);
+
+const NotificationNavigator = () => (
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: { backgroundColor: colors.primary },
+      headerTintColor: colors.white,
+    }}
+  >
+    <Stack.Screen name='Notification' component={TabNavigator} />
+  </Stack.Navigator>
 );
 
 export default NotificationNavigator;
