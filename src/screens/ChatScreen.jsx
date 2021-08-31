0681be73acb74ab11e1b67f9793a5ffec9fe8605
lucifer2355 <dgajjar999@gmail.com>
2021-08-31 21:38:00 +0530

    add chat screen

--- src/screens/ChatScreen.jsx
+++ src/screens/ChatScreen.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 const ChatScreen = () => {
-    return (
-        <View style={styles.screen}>
-            <Text>Chat Screen</Text>
-        </View>
-    )
-}
+  return (
+    <View style={styles.screen}>
+      <Text>Chat Screen</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
-export default ChatScreen
-
+export default ChatScreen;
