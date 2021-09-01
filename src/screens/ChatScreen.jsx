938dacbfe1a5e91132c0ca7230917c4269003d7a
lucifer2355 <dgajjar999@gmail.com>
2021-09-01 12:05:14 +0530

    add send button message style

--- src/screens/ChatScreen.jsx
+++ src/screens/ChatScreen.jsx
@@ -1,13 +1,32 @@
 import React from "react";
-import { StyleSheet, Text, View, TextInput } from "react-native";
-import { hp } from "../config/HeightWidth";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+import { colors } from "../config/colors";
+import { hp, wp } from "../config/HeightWidth";
+import Icons from "../config/Icons";
 
 const ChatScreen = () => {
+  const sendMessage = () => {
+    console.log("send message");
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.chat}></View>
       <View style={styles.textInputView}>
-        <TextInput placeholder='Type a message' style={styles.textInput} />
+        <View style={{ justifyContent: "space-between", flexDirection: "row" }}>
+          <TextInput placeholder='Type a message' style={styles.textInput} />
+          <TouchableOpacity onPress={sendMessage} style={styles.sendButton}>
+            {Icons.materialCommunityIcons("send", 30)}
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -26,15 +45,24 @@ const styles = StyleSheet.create({
     flex: 0.1,
     justifyContent: "flex-end",
     paddingHorizontal: 5,
+    marginBottom: 10,
   },
 
   textInput: {
     borderWidth: 1,
     borderRadius: 20,
     paddingHorizontal: 10,
-    paddingVertical: 15,
-    fontSize: 16,
-    bottom: hp("1%"),
+    fontSize: 18,
+    width: wp("82%"),
+  },
+
+  sendButton: {
+    backgroundColor: colors.primary,
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
